8c056671b4327d0a5d490cb2a6cccc5789645f04
Jiacheng Guo <jg17@illinois.edu>
2020-07-18 16:43:08 -0500

    Database Drop exsit table

--- src/main/java/db/MySQLTableCreation.java
+++ src/main/java/db/MySQLTableCreation.java
@@ -9,12 +9,27 @@ public class MySQLTableCreation {
 			try {
 				// Step 1 Connect to MySQL.
 				System.out.println("Connecting to " + MySQLDBUtil.URL);
-				Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+				Class.forName("com.mysql.cj.jdbc.Driver");
 				Connection conn = DriverManager.getConnection(MySQLDBUtil.URL);
 				
 				if (conn == null) {
 					return;
 				}
+				
+				// Step 2 Drop tables in case they exist.
+				Statement statement = conn.createStatement();
+				String sql = "DROP TABLE IF EXISTS keywords";
+				statement.executeUpdate(sql);
+
+				sql = "DROP TABLE IF EXISTS history";
+				statement.executeUpdate(sql);
+
+				sql = "DROP TABLE IF EXISTS items";
+				statement.executeUpdate(sql);
+
+				sql = "DROP TABLE IF EXISTS users";
+				statement.executeUpdate(sql);
+
 				
 				conn.close();
 				System.out.println("Import done successfully");
