6a452e0acdae399d13ea5b60a8bd41d7f254e257
Jiacheng Guo <jg17@illinois.edu>
2020-07-18 18:35:47 -0500

    Item History

--- src/main/java/rpc/RpcHelper.java
+++ src/main/java/rpc/RpcHelper.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.util.HashSet;
+import java.util.Set;
+import entity.Item;
 
 public class RpcHelper {
 	
@@ -13,10 +16,28 @@ public class RpcHelper {
 		response.setContentType("application/json");
 		response.getWriter().print(array);
 	}
-              // Writes a JSONObject to http response.
+	// Writes a JSONObject to http response.
 	public static void writeJsonObject(HttpServletResponse response, JSONObject obj) throws IOException {		
 		response.setContentType("application/json");
 		response.getWriter().print(obj);
 	}
+	
+	// Convert a JSON Object to Item Object
+	public static Item parseFavoriteItem(JSONObject favoriteItem) {
+		Set<String> keywords = new HashSet<>();
+		JSONArray array = favoriteItem.getJSONArray("keywords");
+		for (int i = 0; i < array.length(); ++i) {
+			keywords.add(array.getString(i));
+		}
+		return Item.builder()
+				.itemId(favoriteItem.getString("item_id"))
+				.name(favoriteItem.getString("name"))
+				.address(favoriteItem.getString("address"))
+				.url(favoriteItem.getString("url"))
+				.imageUrl(favoriteItem.getString("image_url"))
+		        .keywords(keywords)
+		        .build();
+		}
+
 
 }
